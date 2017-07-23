4d086741200c8f1c3e21e94d4eab7cdec6a9f8f8
commoninvestor <jethromak0@gmail.com>
2017-07-23 11:28:04 -0400

    Working Copy Aurora Plugin

--- AuroraPluginTemplate/src/AuroraPlugin.cpp
+++ AuroraPluginTemplate/src/AuroraPlugin.cpp
@@ -1,12 +1,9 @@
 /*
  Copyright 2017 Nanoleaf Ltd.
-
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
  http://www.apache.org/licenses/LICENSE-2.0
-
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +17,7 @@
 #include "DataManager.h"
 #include "PluginFeatures.h"
 #include "Logger.h"
+#include <stdlib.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -33,6 +31,20 @@ void pluginCleanup();
 }
 #endif
 
+LayoutData* layoutData;
+FrameSlice_t* frameSlices = NULL;
+int nFrameSlices = 0;
+int prevSliceLit = 0;
+static int currentAuroraRotation = 0;
+
+static bool didLoadPluginByLeo = false;
+
+RGB_t * colorPalette = NULL;
+int nColors = 0;
+RGB_t sliceColor;
+int colorIndex = 1;
+RGB_t baseColor;
+
 /**
  * @description: Initialize the plugin. Called once, when the plugin is loaded.
  * This function can be used to enable rhythm or advanced features,
@@ -41,9 +53,28 @@ void pluginCleanup();
  * Any allocation, if done here, should be deallocated in the plugin cleanup function
  *
  */
+
 void initPlugin() {
+	//do allocation here
 	enableEnergy();
 	enableBeatFeatures();
+
+	getColorPalette(&colorPalette, &nColors);
+
+	layoutData = getLayoutData();
+
+	//quantizes the layout into frameslices. See SDK documentation for more information
+	getFrameSlicesFromLayoutForTriangle(layoutData, &frameSlices, &nFrameSlices,
+			currentAuroraRotation);
+
+	baseColor = {255, 255, 255};
+	if (nColors == 0) {
+		sliceColor = {0, 0, 0};
+	}
+	else {
+		sliceColor = colorPalette[0];
+	}
+	//y = (rand() % (maxValue - minValue + 1)) + minValue;
 
 }
 
@@ -63,6 +94,57 @@ void initPlugin() {
  * @param sleepTime: specify interval after which this function is called again, NULL if sound visualization plugin
  */
 void getPluginFrame(Frame_t* frames, int* nFrames, int* sleepTime) {
+#define SKIP_COUNT 1
+	// a tiny block of code that allows the developer to skips calls of this function.
+	//if this plugin wishes to run only every 150ms for instance, the skip count would be set to 2
+	static int cnt = 0;
+	if (cnt < SKIP_COUNT) {
+		cnt++;
+		return;
+	}
+	cnt = 0;
+
+	int sliceToLight;
+	int frameIndex = 0;
+	PRINTLOG("a function called\n");
+	PRINTLOG("Max slice %d", nFrameSlices);
+	if (nColors >= 2) {
+		if (colorIndex > nColors) {
+			colorIndex = 1;
+		}
+		sliceColor = colorPalette[colorIndex++];
+	}
+	PRINTLOG("\nSlicecolors: R: %d, G: %d, B: %d\n", sliceColor.R, sliceColor.G,
+			sliceColor.B);
+
+	for (int i = 0; i < nFrameSlices; i++) {
+		for (unsigned int j = 0; j < frameSlices[i].panelIds.size(); j++) {
+			PRINTLOG("i: %d j: %d\n", i, j);
+			frames[j].panelId = frameSlices[i].panelIds[j];
+			frames[j].r = baseColor.R;
+			frames[j].g = baseColor.G;
+			frames[j].b = baseColor.B;
+			frames[j].transTime = 1;
+			frameIndex++;
+		}
+	}
+	*nFrames = frameIndex;
+	if (getIsBeat()) {
+		PRINTLOG("beatFunction \n");
+		do {
+			sliceToLight = (rand() % nFrameSlices);
+		} while (sliceToLight == prevSliceLit);
+		PRINTLOG("Random number: %d \n", sliceToLight);
+		for (unsigned int j = 0; j < frameSlices[sliceToLight].panelIds.size();
+				j++) {
+			frames[j].panelId = frameSlices[sliceToLight].panelIds[j];
+			frames[j].r = sliceColor.R;
+			frames[j].g = sliceColor.G;
+			frames[j].b = sliceColor.B;
+			frames[j].transTime = 1;
+		}
+		prevSliceLit = sliceToLight;
+	}
 
 }
 
@@ -71,5 +153,6 @@ void getPluginFrame(Frame_t* frames, int* nFrames, int* sleepTime) {
  * Do all deallocation for memory allocated in initplugin here
  */
 void pluginCleanup() {
-	//do deallocation here
+//do deallocation here
+	freeFrameSlices(frameSlices);
 }
